d350f197e62186b4167b1394a7182f22d89968be
Raul Sanchez R. <raul.sr92@gmail.com>
2026-09-23 09:47:05 -0500

    Clase 111 CRON Tasks

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,24 @@
+import { CronJob } from "cron"
 
+ 
 export class Server{
 
     public static start(){
         console.log('Server started....')
+
+        //instancia de cronjob
+
+            const job = new CronJob(
+                '*/3 * * * * *', // cronTime
+                () => {
+
+                    const date = new Date();
+
+                    console.log('Este mensaje aparece cada 2 segundos', date);
+                },
+            );      
+            
+            job.start();
     }
 }
 
